b99f95eff0849ec093d03045d33a034cf60c9069
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-12-31 10:17:47 +0700

    feature: Add navbar and search doc

--- src/app/exercises/layout.tsx
+++ src/app/exercises/layout.tsx
@@ -9,7 +9,7 @@ export default function ExercisesLayout({
 }>) {
   return (
     <div>
-      <header className="bg-white w-full h-12 shadow-md px-4 py-2 sticky top-0 z-50">
+      <header className="bg-white w-full h-12 shadow-md px-4 py-1 sticky top-0 z-[100]">
         <Navbar />
       </header>
       <div className="flex">
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,52 @@
+"use client";
+
+import { Search } from "lucide-react";
+import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.metaKey && event.key === "k") {
+        event.preventDefault();
+        setIsOpen((prev) => !prev);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
   return (
-    <nav className="flex space-x-10 ">
-      <p>logo</p>
-      <div className="bg-slate-200 w-48 h-8 rounded-xl">Search</div>
+    <nav className="flex space-x-10 items-center">
+      <Image
+        src="/logos/world.svg"
+        width={40}
+        height={40}
+        alt="Picture of the author"
+      />
+      <div
+        onClick={() => setIsOpen(true)}
+        className="bg-slate-200 hover:cursor-pointer w-48 h-8 pl-2 rounded-xl flex items-center justify-between"
+      >
+        <div className="flex gap-2">
+          <Search width={16} />
+          <p>Search</p>
+        </div>
+        <span className="px-3">⌘K</span>
+      </div>
+      <SearchDialog isOpen={isOpen} setIsOpen={setIsOpen} />
       <ul>
         <li>
           {" "}
@@ -10,5 +54,32 @@ export default function Navbar() {
         </li>
       </ul>
     </nav>
+  );
+}
+
+type SearchDialogProps = {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+};
+export function SearchDialog({ isOpen, setIsOpen }: SearchDialogProps) {
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader className="hidden">
+          <DialogTitle>Search</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 ">
+          <div className="items-center  w-full flex">
+            <Search width={16} />
+            <Input
+              id="name"
+              type="text"
+              placeholder="Search documentation"
+              className="focus:outline-none border-none  p-2"
+            />
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
